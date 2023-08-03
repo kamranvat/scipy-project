54a67df182da77c388cdc9e8d942ff3d8f0dae5e
Kamran Vatankhah <kamran.vat@protonmail.com>
2023-08-03 16:36:21 +0200

    log to csv implemented

--- testenv.py
+++ testenv.py
@@ -1,7 +1,7 @@
 """
 Just for testing purposes.
 Run a gym environment with a stable_baselines3 policy.
-Later, test logging the data.
+Log the data to csv
 """
 
 import gymnasium as gym
@@ -10,17 +10,24 @@ import gymnasium as gym
 import matplotlib.pyplot as plt
 import datetime
 from stable_baselines3 import A2C
+from stable_baselines3.common.logger import configure
+
+# Configure logging
+tmp_path = "./logs/"
+new_logger = configure(tmp_path, ["stdout", "csv"])
+
+# Set model and train
+model = A2C("MlpPolicy", "CartPole-v1", verbose=1)
+model.set_logger(new_logger)
+model.learn(10000)
+
+
+print("Run environment and render: ")
 
 env = gym.make("CartPole-v1", render_mode = 'human')
-#obs_space = env.observation_space
-#act_space = env.action_space
-#obs = env.reset()
-
-model = A2C("MlpPolicy", env, verbose=1, tensorboard_log = "./a2c_cartpole_tensorboard/")
-model.learn(total_timesteps=10_000)
-
 vec_env = model.get_env()
 obs = vec_env.reset()
+
 for i in range(1000):
     action, _state = model.predict(obs, deterministic=True)
     obs, reward, done, info = vec_env.step(action)
