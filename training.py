ac1f25db8130304ba5cc8db5558560919a32e637
Kamran Vatankhah <kamran.vat@protonmail.com>
2023-08-04 13:22:08 +0200

    built basic training.py

--- training.py
+++ training.py
@@ -1,33 +1,31 @@
 """Contains everything required to train models after being passed a list of dicts with model names"""
 
-# TODO  functions to pull the names out of the dicts, call the right algos
-
-import gymnasium as gym
-#import numpy as np
-#import tensorflow as tf
-import matplotlib.pyplot as plt
-import datetime
-
 from stable_baselines3.common.logger import configure
 
-# TODO double check the imported algos here on the website of sd3
 from stable_baselines3 import A2C
 from stable_baselines3 import DQN
 from stable_baselines3 import PPO
 
 
-
-def call_active_models(agent_list):
+def train_active_models(agent_list):
     """
     Takes a list of dictionaries that include a "name" key and value. 
     Calls training for each model that is named in the list.
     """
+
+    # Configure logging
+    log_path = "./logs/"
+    csv_logger = configure(log_path, ["stdout", "csv"])
+
+    # TODO allow user to change number of episodes
+
     for agent in agent_list:
         if agent.get("active") == True:
             policy_name = agent.get("name")
             #environment_name = agent.get("environment")
             environment_name = "CartPole-v1"
-            model_args = agent.get("model_args", {})
+            #TODO add optimal hyperparameters to model args to get to 6 models
+            #model_args = agent.get("model_args", {})
 
             # "Translate" from str to class
             policy_classes = {
@@ -38,11 +36,15 @@ def call_active_models(agent_list):
 
             if policy_name in policy_classes:
                 policy_class = policy_classes[policy_name]
-                model = policy_class(**model_args, env=environment_name, verbose=1)
+                #model = policy_class(**model_args, env=environment_name, verbose=1)
+                model = policy_class(env=environment_name, verbose=1)
 
                 # Call the policy here or use it as needed 
                 # TODO potentially add something here idk
-                print(f"Policy {policy_name} is active. Model: {model}")
+                print(f"Policy {policy_name} is active. Model: {model}. Training now...")
+                model.set_logger(csv_logger)
+                model.learn(5000)
+
             else:
                 print(f"Policy {policy_name} is active, but the class is not defined.")
 
