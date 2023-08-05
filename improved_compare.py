26811810e1f84f76b00a8c89e84a4d388e72c665
Christian-Meissner <61634059+Christian-Meissner@users.noreply.github.com>
2023-08-05 18:58:00 +0200

    Add files via upload

--- improved_compare.py
+++ improved_compare.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from statistics import mean 
 
 def compare(model_list):
     """
@@ -11,7 +12,20 @@ def compare(model_list):
             (name, description, active)
 
     """
+    # extract the logged performance measurements and model titles seperately from the dictionary
     logs, titel = read_logs(model_list)
+
+    # creation of a variable for iteration over the model titels  
+    model_num = 0
+    
+    # creation of a list for the models average rewards
+    avg_reward = []
+    for log in logs:
+        print(f"\n\n\nThis is an overview over the logged parameters from the {titel[model_num]} run:\n")
+        print(log.head())
+        avg_reward.append(mean(log["rollout/ep_rew_mean"])) 
+        print(f"\n{titel[model_num]}s average reward over all timesteps is {avg_reward[model_num]}.")
+        model_num += 1
 
     print(""" 
         The environment describes the world the agent is located in and changes its state based 
@@ -23,14 +37,15 @@ def compare(model_list):
     # create a figure showing the mean episode reward over time for both models
     fig, ax = plt.subplots(nrows = 1, ncols = 1)
 
-    model_num = 0
-    # iterate over the list of logs
+    # reset of the iteration variable
+    model_num = 0 
+    # iterate over the list of logs and display them in one plot
     for log in logs:
         ax.plot(log["time/total_timesteps"], log['rollout/ep_rew_mean'], label=titel[model_num])
         ax.set(ylabel="Mean Episode Rewards", xlabel="Number of Timesteps", title="Mean Episode Rewards depending on Timesteps")
-        ax.legend()
         model_num += 1
-
+        ax.legend()    
+    plt.xlim(left=0)
     plt.show()
 
 
@@ -46,11 +61,13 @@ def read_logs(model_list):
 
     Returns:
         logs_list: list of the logs of active models, from the logs folder
+        titel_list: list of model titles
     """
 
     logs_list = []
     titel_list = []
 
+    # iterate over the active models in the dictionary and get the logs and names
     for model in model_list:
         if model.get("active"):
             model_name = model.get("name")
